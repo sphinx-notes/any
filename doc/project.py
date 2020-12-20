4980136d6f4d3cf3bc728e15937979ab2bdd9fc7
Shengyu Zhang <i@silverrainz.me>
2020-12-20 23:12:29 +0800

    Include documentation in wheel

--- doc/project.py
+++ doc/project.py
@@ -5,7 +5,7 @@ Project Meta Information
 '''
 
 name = 'sphinxnotes-any'
-version = '1.0a0'
+version = '1.0a1'
 github_user = 'sphinx-notes'
 github_repo = 'any'
 url = 'https://github.com/' + github_user + '/' + github_repo
