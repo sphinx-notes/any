72befab74aed610b7dd3c9f2700c05317bf44c26
Shengyu Zhang <i@silverrainz.me>
2024-09-09 14:58:40 +0800

    refactor: Split a sphinxnotes.jinja extension

---
+++ src/sphinxnotes/jinja/context.py
@@ -0,0 +1,9 @@
+"""
+sphinxnotes.jinja.rstctx
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Sphinx extension entrypoint of sphinxnotes-jinja.
+
+:copyright: Copyright 2024 Shengyu Zhang
+:license: BSD, see LICENSE for details.
+"""
